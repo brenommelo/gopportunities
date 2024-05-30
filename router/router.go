58769cb54c0d3da8cc06f58f02efabdea8f7e442
breno <brenommelo@gmail.com>
2024-05-30 17:33:52 -0300

    feat: add routing logic

--- router/router.go
+++ router/router.go
@@ -3,14 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+
 	//inicializa o router utilizando a configs default do gin
 	router := gin.Default()
-	//define rotas
-	router.GET("/ping", func(c *gin.Context) { //esse funcao seria um handler
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	//Iniciando rotas
+	initializeRoutes(router)
 
 	//aqui toda a api
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
